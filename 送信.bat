bda13fd2ab200a729ce40e7630977766b648cd60
Nanasi339 <49983665+Nanasi339@users.noreply.github.com>
2024-12-01 23:59:45 +0900

    クイック更新テスト2

--- 送信.bat
+++ 送信.bat
@@ -1,6 +1,7 @@
 rem @echo off
 git add .
-git commit -m "クイック更新テスト"
+git commit -m "クイック更新テスト2"
+git remote rm origin
 git remote add origin https://github.com/Nanasi339/BLANK.git
-git push -u origin
+git push origin master
 pause
