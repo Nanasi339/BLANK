79eed53cfcb3b972036288baef8086434cbd7093
Nanasi339 <49983665+Nanasi339@users.noreply.github.com>
2024-12-07 09:35:08 +0900

    基礎開発中

--- 送信.bat
+++ 送信.bat
@@ -1,6 +1,6 @@
-echo off
+@echo off
 git add .
-git commit -m "クイック更新"
+git commit -m "基礎開発中"
 git remote rm origin
 git remote add origin https://github.com/Nanasi339/BLANK.git
 git push origin master
