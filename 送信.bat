f42a4f68c47083d8f313305fd84f8ea410af97a8
Nanasi339 <49983665+Nanasi339@users.noreply.github.com>
2024-12-08 16:21:53 +0900

    JavaScriptゲーム開発中

--- 送信.bat
+++ 送信.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "Šî‘bŠJ”­’†"
+git commit -m "JavaScriptƒQ[ƒ€ŠJ”­’†"
 git remote rm origin
 git remote add origin https://github.com/Nanasi339/BLANK.git
 git push origin master
